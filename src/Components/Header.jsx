c529e18538fb64bbb76b0d217aee7d4d5a0d9b78
Vivexa Tech <vivexatech@gmail.com>
2025-12-24 10:13:52 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,68 +13,85 @@ export default function Header() {
   );
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
-      <div className="container">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+  <div className="container">
 
-        <Link className="navbar-brand" to="/">
-          <i className="fas fa-ghost me-2"></i>V Game Store
-        </Link>
+    <Link className="navbar-brand" to="/">
+      <i className="fas fa-ghost me-2"></i>V Game Store
+    </Link>
 
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto align-items-center">
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#mainNavbar"
+      aria-controls="mainNavbar"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-            <li className="nav-item">
-              <Link to="/" className="nav-link pe-3">STORE</Link>
-            </li>
+    <div className="collapse navbar-collapse" id="mainNavbar">
 
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle pe-3"
-                to="/Library"
-                data-bs-toggle="dropdown"
-              >
-                LIBRARY
+      <ul className="navbar-nav ms-auto align-items-lg-center">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">STORE</Link>
+        </li>
+
+        <li className="nav-item dropdown">
+          <Link
+            className="nav-link dropdown-toggle"
+            to="/Library"
+            role="button"
+            data-bs-toggle="dropdown"
+          >
+            LIBRARY
+          </Link>
+
+          <ul className="dropdown-menu dropdown-menu-dark">
+            <li>
+              <Link className="dropdown-item" to="/Library">
+                All Store
               </Link>
-
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/Library">
-                    All Store
-                  </Link>
-                </li>
-                {uniqueCategories.map(item => (
-                  <li key={item.storeID}>
-                    <Link
-                      className="dropdown-item"
-                      to={`/Library/${item.storeID}`}
-                    >
-                      Store ID: {item.storeID}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
             </li>
+
+            {uniqueCategories.map(item => (
+              <li key={item.storeID}>
+                <Link
+                  className="dropdown-item"
+                  to={`/Library/${item.storeID}`}
+                >
+                  Store ID: {item.storeID}
+                </Link>
+              </li>
+            ))}
           </ul>
+        </li>
+      </ul>
 
-          <div className="position-relative ms-3" style={{ width: "300px" }}>
-            <form className="d-flex search-form">
-              <div className="input-group">
-                <span className="input-group-text bg-dark border-secondary text-muted">
-                  <i className="fas fa-search"></i>
-                </span>
-                <input
-                  className="form-control form-control-dark"
-                  type="search"
-                  placeholder="Search games..."
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                /><SearchBox search={search} />
-              </div>
-            </form>
-          </div>
+      <form className="d-flex ms-lg-3 mt-3 mt-lg-0 position-relative">
+        <div className="input-group">
+          <span className="input-group-text bg-dark border-secondary text-muted">
+            <i className="fas fa-search"></i>
+          </span>
 
+          <input
+            className="form-control form-control-dark"
+            type="search"
+            placeholder="Search games..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
-      </div>
-    </nav>
+
+        <SearchBox search={search} />
+      </form>
+
+    </div>
+  </div>
+</nav>
+
   );
 }
